3552747e34d291e24acd2ee7caaae37a3865b63e
JoseEnriqueMendo <dinofiesta123@gmail.com>
2022-12-08 16:26:32 -0500

    union 101

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const usuariorouter= require("./user");
 const apuestarouter= require("./apuesta");
-
+const facturarouter= require("./factura");
 
 router.use("/usuario",usuariorouter);
 router.use("/apuesta",apuestarouter);
-
+router.use("/factura",facturarouter);
 module.exports=router;
 
